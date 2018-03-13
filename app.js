381e4ac3afc048349e60b4d4f8ac65410801f03d
jromano8954 <jromano6556@gmail.com>
2018-03-12 20:38:38 -0400

    Adding Express functionality

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+//Setting up express for our app to listen
+var express = require('express');
+var app = express();
+
+app.set('port',3000);
+
+var server = app.listen(app.get('port'),function(){
+    var port = server.address().port;
+    console.log('Magic happends on port '+port);
+});
+
+/*
 require('./instantHello');
 //Require the import/include of Node JS 
 var goodbye = require('./talk/goodbye');
@@ -5,8 +17,10 @@ var talk = require('./talk');
 var question = require("./talk/question");
 
 
+
 var answer = question.ask("What is the meaning of life");
 console.log(answer);
 talk.intro();
 talk.hello("someone");
 goodbye();
+*/
