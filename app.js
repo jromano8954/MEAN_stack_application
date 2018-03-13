373af6a5a609cfa55388715b5901ceceef09a567
jromano8954 <jromano6556@gmail.com>
2018-03-12 21:23:44 -0400

    Updating Express functionality

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 //Setting up express for our app to listen
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port',3000);
+
+//req = request object, res = response object 
+//send homepage file
+app.get('/',function(req,res){
+    console.log("GET the homepage");
+    //Sending a response with setting a http status code 
+    res
+    .status(200)
+    .sendFile(path.join(__dirname,'public','index.html'));
+    //.send('Express yourself');
+});
+//send json file
+app.get('/json',function(req,res){
+    console.log("GET the json");
+    //Sending a response with setting a http status code 
+    res
+    .status(200)
+    .json( {"jsonData" : true} );
+});
+//send a file
+app.get('/file',function(req,res){
+    console.log("GET the file");
+    //Sending a response with setting a http status code 
+    res
+    .status(200)
+    .sendFile(path.join(__dirname, 'app.js'));
+});
 
 var server = app.listen(app.get('port'),function(){
     var port = server.address().port;
