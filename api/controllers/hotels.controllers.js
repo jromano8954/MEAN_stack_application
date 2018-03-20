c23291598ac738f32377282c38463b13ea33dbcb
jromano8954 <jromano6556@gmail.com>
2018-03-20 12:38:23 -0400

    Added a way to add mongo docs via POST

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,4 +1,5 @@
 var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req,res){
@@ -6,12 +7,27 @@ module.exports.hotelsGetAll = function(req,res){
     var db = dbconn.get();
     var collection = db.collection('hotels');
 
-    var docs = collection.find();
+    var offset = 0;
+    var count = 5;
 
-    console.log("Found hotels", docs);
-    res
-        .status(200)
-        .json(docs);
+    if(req.query && req.query.offset){
+        offset = parseInt(req.query.offset, 10);
+    }
+
+    if(req.query && req.query.count){
+        count = parseInt(req.query.count, 10);
+    }
+
+    collection
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function(err,docs){
+            console.log("Found hotels",docs);
+            res
+                .status(200)
+                .json(docs);
+        });
 
     console.log('db',db);
 
@@ -21,16 +37,7 @@ module.exports.hotelsGetAll = function(req,res){
     // //Sending a response with setting a http status code 
 
     // //Using values from querystring to set amount returned
-    // var offset = 0;
-    // var count = 5;
-
-    // if(req.query && req.query.offset){
-    //     offset = parseInt(req.query.offset, 10);
-    // }
-
-    // if(req.query && req.query.count){
-    //     count = parseInt(req.query.count, 10);
-    // }
+    //moved code to before find call 
 
     // var returnData = hotelData.slice(offset,offset+count);
 
@@ -40,20 +47,45 @@ module.exports.hotelsGetAll = function(req,res){
 };
 //Controller for single hotel
 module.exports.hotelsGetOne = function(req,res){
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
     //Takes the hotelId from request object and stores it 
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
+    // var thisHotel = hotelData[hotelId];
     console.log("GET hotelId", hotelId);
-    //Sending a response with setting a http status code 
-    res
-        .status(200)
-        .json( thisHotel);
+
+    collection
+        .findOne({
+            _id : ObjectId(hotelId)
+        },function(err,doc){
+            //Sending a response with setting a http status code 
+            res
+             .status(200)
+             .json( doc);
+        });
+    
 };
 //Controller to add a hotel
 module.exports.hotelsAddOne = function(req,res){
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    var newHotel;
+
     console.log("POST new hotel");
-    console.log(req.body);
-    res
-        .status(200)
-        .json( req.body);
+    if(req.body && req.body.name && req.body.stars){
+        newHotel = req.body;
+        newHotel.stars = parseInt(req.body.stars,10);
+        console.log(newHotel);
+        collection.insertOne(newHotel,function(err,response){
+            console.log(response.ops);
+            res
+            .status(201)
+            .json( response.ops);
+        });   
+    }else{
+        res
+            .status(400)
+            .json({message : "Required data missing from body"});
+    }
+
 };
