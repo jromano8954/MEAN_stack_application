d1f1744ffbafa6a88c42c2d0e67b0ddac748e99b
jromano8954 <jromano6556@gmail.com>
2018-03-23 12:58:19 -0400

    Using Mongoose to get sub-documents out of database (reviews)

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var ctrlHotels = require('../controllers/hotels.controllers.js');
+var ctrlReviews = require('../controllers/reviews.controllers.js');
 
 router
     .route('/hotels')
@@ -23,5 +24,15 @@ router
         .json( {"jsonData" : "POST received"});
     })
     */
+
+// Review Routes
+router
+    .route('/hotels/:hotelId/reviews')
+    .get(ctrlReviews.reviewsGetAll);
+//URL Parameters hotelId
+router
+    .route('/hotels/:hotelId/reviews/:reviewId')
+    .get(ctrlReviews.reviewsGetOne);
+
 
 module.exports = router;
---
+++ commands.js
@@ -0,0 +1,13 @@
+db.hotels.update(
+    {
+        "name":"Grand Hotel Palatino"
+    },
+    {
+        $set:{
+            "reviews.1._id" : ObjectId()
+        }
+    },
+    {
+        //multi:true
+    }
+)
