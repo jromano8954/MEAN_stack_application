aeba73661e99bc4e65eb0883a9473c530703f2df
jromano8954 <jromano6556@gmail.com>
2018-03-23 12:19:48 -0400

    Updating controllers to work with mongoose

--- api/controllers/hotels.controllers.old.js
+++ api/controllers/hotels.controllers.old.js
@@ -1,8 +1,40 @@
 var mongoose = require('mongoose');
 var Hotel = mongoose.model('Hotel');
 
-//Controller to get mulitple/all hotels
+/* No longer going to need this instead use mongoose and model
+var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
+var hotelData = require('../data/hotel-data.json');
+*/
 module.exports.hotelsGetAll = function(req,res){
+    /*Native Driver way to retrive data
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+
+    var offset = 0;
+    var count = 5;
+
+    if(req.query && req.query.offset){
+        offset = parseInt(req.query.offset, 10);
+    }
+
+    if(req.query && req.query.count){
+        count = parseInt(req.query.count, 10);
+    }
+
+    collection
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function(err,docs){
+            console.log("Found hotels",docs);
+            res
+                .status(200)
+                .json(docs);
+        });
+
+    console.log('db',db);
+    */
 
     var offset = 0;
     var count = 5;
@@ -24,17 +56,34 @@ module.exports.hotelsGetAll = function(req,res){
             res
                 .json(hotels);
         });
+
+    // console.log("GET the hotel");
+    // //Prints the properties of the query string from URL 
+    // console.log(req.query);
+    // //Sending a response with setting a http status code 
+
+    // //Using values from querystring to set amount returned
+    //moved code to before find call 
+
+    // var returnData = hotelData.slice(offset,offset+count);
+
+    // res
+    //     .status(200)
+    //     .json(returnData);
 };
 //Controller for single hotel
 module.exports.hotelsGetOne = function(req,res){
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
     //Takes the hotelId from request object and stores it 
     var hotelId = req.params.hotelId;
     // var thisHotel = hotelData[hotelId];
     console.log("GET hotelId", hotelId);
 
-    Hotel
-        .findById(hotelId)
-        .exec(function(err,doc){
+    collection
+        .findOne({
+            _id : ObjectId(hotelId)
+        },function(err,doc){
             //Sending a response with setting a http status code 
             res
              .status(200)
